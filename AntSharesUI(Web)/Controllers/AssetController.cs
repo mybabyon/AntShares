7bb664d1ba7eff9c7c9249728f8b4eedba138fa9
chenzhitong <chenzhitong12@qq.com>
2015-12-21 17:04:17 +0800

    将本地的AntSharesUI(Web)同步到远程GitHub的UI分支

---
+++ AntSharesUI(Web)/Controllers/AssetController.cs
@@ -0,0 +1,30 @@
+using System.Linq;
+using Microsoft.AspNet.Mvc;
+using Microsoft.AspNet.Mvc.Rendering;
+using Microsoft.Data.Entity;
+using AntSharesUI_Web_.Models;
+
+namespace AntSharesUI_Web_.Controllers
+{
+    public class AssetController : Controller
+    {
+
+        // GET: Asset/MyAsset
+        public IActionResult MyAsset()
+        {
+            return View();
+        }
+
+        // GET: Asset/Details?id=1
+        public IActionResult Details(int id)
+        {
+            return View();
+        }
+
+        // GET: Asset/Transfer
+        public IActionResult Transfer()
+        {
+            return View();
+        }
+    }
+}

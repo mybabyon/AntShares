315b37ecca63eb1b0a4c716c9d5da2d38e214356
chenzhitong <chenzhitong12@qq.com>
2015-12-22 15:39:03 +0800

    完成钱包文件的上传；新增错误页面；修复了验证控件的错误；

--- AntSharesUI(Web)/Controllers/WalletController.cs
+++ AntSharesUI(Web)/Controllers/WalletController.cs
@@ -3,6 +3,9 @@ using Microsoft.AspNet.Mvc;
 using Microsoft.AspNet.Mvc.Rendering;
 using Microsoft.Data.Entity;
 using AntSharesUI_Web_.Models;
+using Microsoft.AspNet.Http;
+using Microsoft.Net.Http.Headers;
+using System.Threading.Tasks;
 
 namespace AntSharesUI_Web_.Controllers
 {
@@ -15,8 +18,39 @@ namespace AntSharesUI_Web_.Controllers
             return View();
         }
 
+        //http://www.cnblogs.com/redmoon/p/4601133.html
+        [HttpPost]
+        public async Task<IActionResult> Upload(IFormFile wallet, string Password)
+        {            
+            if (wallet != null)
+            {
+                var fileName = ContentDispositionHeaderValue.Parse(wallet.ContentDisposition).FileName.Trim('"');
+                if (fileName.EndsWith(".db3"))// 只保存db3文件
+                {
+                    if (Password.Length > 3)//验证密码正确性
+                    {
+                        var filePath = "WalletFile.db3";
+                        await wallet.SaveAsAsync(filePath);
+                        return RedirectToAction("MyWallet");
+                    }
+                    else
+                    {
+                        return RedirectToAction("Error");
+                    }
+                }
+            }
+            
+            return RedirectToAction("Open");
+        }
+
         // GET: Wallet/Open
         public IActionResult Open()
+        {
+            return View();
+        }
+
+        // GET: Wallet/Error
+        public IActionResult Error()
         {
             return View();
         }

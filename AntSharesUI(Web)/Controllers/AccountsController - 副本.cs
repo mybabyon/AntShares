91912b64ae331057cba6ab1ac1a9bd47b2ffbf93
chenzhitong <chenzhitong12@qq.com>
2016-01-04 16:32:43 +0800

    修复源代码控制的问题（1）

---
+++ AntSharesUI(Web)/Controllers/AccountsController - 副本.cs
@@ -0,0 +1,32 @@
+﻿using System.Linq;
+using Microsoft.AspNet.Mvc;
+using Microsoft.AspNet.Mvc.Rendering;
+using Microsoft.Data.Entity;
+using AntSharesUI_Web_.Models;
+
+namespace AntSharesUI_Web_.Controllers
+{
+    public class AccountsController : Controller
+    {
+
+
+        // GET: Accounts/AccountList
+        public IActionResult AccountList()
+        {
+            ViewBag.Accounts = new string[] { "我常用的账户", "私人账户", "公司账户" };
+            return View();
+        }
+
+        // GET: Accounts/Account?id=1
+        public IActionResult Account(int id)
+        {
+            return View();
+        }
+
+        // GET: Accounts/Create
+        public IActionResult Create(int id)
+        {
+            return View();
+        }
+    }
+}
---
+++ AntSharesUI(Web)/Models/Account - 副本.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AntSharesUI_Web_.Models
+{
+    public class Account
+    {
+        public int Id { get; set; }
+
+        [Required(ErrorMessage = "账户名不能为空")]
+        public string Name { get; set; }
+
+    }
+}

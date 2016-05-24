d17d1ffa515247f6e2067cdb2788aa8784b8cd49
陈志同 <chenzhitong12@hotmail.com>
2016-05-24 18:46:49 +0800

    加入千分符

--- AntSharesApp/scripts/AntShares/Function.ts
+++ AntSharesApp/scripts/AntShares/Function.ts
@@ -162,3 +162,36 @@ function ToUint8Array(array: Array<Uint8Array>): Uint8Array
     }
     return result;
 }
+
+
+function convert_keyup(obj)
+{
+    let n = obj.value.replace(/,/g, "") as string;
+
+    let str = n.split('.');
+    if (str.length > 2)
+        return;
+    let z = str[0].split('').reverse().join('');
+    let z1 = "";
+    for (let i = 0; i < z.length; i++)
+    {
+        z1 = z1 + z[i];
+        if ((i + 1) % 3 == 0 && i < z.length - 1)
+            z1 = z1 + ",";
+    }
+    z1 = z1.split('').reverse().join('');
+
+    let result = z1;
+    if (str.length == 2)
+    {
+        let f = str[1];
+        result = result + "." + f;
+    }
+    obj.value = result;
+    console.log(result);
+}
+
+function convert_keydown(obj)
+{
+    setTimeout(() => { convert_keyup(obj) }, 10);
+}
